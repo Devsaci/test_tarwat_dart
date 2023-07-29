4930c968c0cd486e7a4d191d272d562741fb725b
saci zakaria <sacideveloppeur@gmail.com>
2023-07-29 19:56:07 +0200

    List java

---
+++ javatest/listjava.java
@@ -0,0 +1,18 @@
+package javatest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listjava {
+    static List<String> strings = new ArrayList<>();
+  
+      public static void main(String[] args) {
+    strings.add("un");
+    strings.add("deux");
+    strings.add("trois");
+    for (String string : strings) {
+      System.out.println(string);
+    }
+  }
+    
+}
